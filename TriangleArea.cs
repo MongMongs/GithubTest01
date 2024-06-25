2732f897d085dab7178adb0aa04b3c2a96bd1faa
MongMongs <102705579+MongMongs@users.noreply.github.com>
2024-06-25 17:24:12 +0900

    Add files via upload

---
+++ TriangleArea.cs
@@ -0,0 +1,20 @@
+namespace ConsoleApp6
+{
+    internal class TriangleArea
+    {
+        static void Main(string[] args)
+        {
+            //1. 변수선언 및 입력부
+            Console.Write("가로값을 입력해 주세요:");
+            int width = int.Parse(Console.ReadLine());
+            Console.Write("세로값을 입력해 주세요:");
+            int height = int.Parse(Console.ReadLine ());
+
+            //2. 알고리즘 수식
+            int result = (width * height) / 2;
+            
+            //3. 출력부
+            Console.WriteLine($"넓이는{result}입니다");
+        }
+    }
+}
